206346fe901d83af1cd55779060a8abe734952f8
AhmedElnaggar <es-ahmed.elnagar2027@alexu.edu.eg>
2025-12-04 00:40:44 +0200

    last_modify-4/12

--- Robot.ino
+++ Robot.ino
@@ -8,19 +8,33 @@ void setup()
     L_leg_setup(10);       // left leg servo connected to pin 10
     ultrsnc_head_setup(7 , 6);   // ultrasonic sensor echo pin connected to pin 7 , trig pin connected to pin 6
     robot_init();
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    distance = read_distance();
-    while(distance > 5)   // rotation until an obstacle is detected (test only)
+     while(distance < 10)
     {
-        rotate_1_step(RIGHT_LEG , 500);  // rotating to the left using right leg with delay of 500 milliseconds between steps
         distance = read_distance();
     }
-    while(distance > 5)   // move while distance is more than 5 cm
+    delay(2000);
+    while(distance >= 10)   // rotation until an obstacle is detected (test only)
+    {
+        rotate_1_step(LEFT_LEG , 500);  // rotating to the left using right leg with delay of 500 milliseconds between steps
+        distance = read_distance();
+        Serial.println(distance);
+        delay(100);
+    }
+    while(distance < 10)
+    {
+        distance = read_distance();
+    }
+    while(distance >= 10)   // move while distance is more than 10 cm
     {
         move_2_steps(500);   // moving the robot 2 steps with delay of 500 milliseconds between steps
         distance = read_distance();
+        Serial.println(distance);
+        delay(100);
     }
+    
 }
